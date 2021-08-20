2877150a103ece6228522b0715f58cdd5af3b1f2
Mahmoud Mostafa <mahmoud309mostafa@gmail.com>
2021-08-20 17:51:10 +0200

    ''

---
+++ src/Component/SearchButton.js
@@ -0,0 +1,18 @@
+import React , { Component } from "react"
+
+class SearchButton extends Component {
+    render() {
+        return (
+
+            <div className="open-search">
+                <button 
+                onClick={() => 
+                    { this.props.showSearchPage(true)}}
+                >Add a book
+                    </button>
+            </div>
+        )
+    }
+}
+
+export default SearchButton;
